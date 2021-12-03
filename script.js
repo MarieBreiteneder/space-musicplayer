4e6b6875b76e2e6ab7ccc2d0cd1233446268d345
MarieBreiteneder <95467788+MarieBreiteneder@users.noreply.github.com>
2021-12-03 11:34:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+//Jupiter
+function myFunctionA() {
+    var x = document.getElementById("jupitermp3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+//White earth
+function myFunctionB() {
+    var x = document.getElementById("white_earthmp3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+//Mond
+function myFunctionC() {
+    var x = document.getElementById("mondmp3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+//Red planet
+function myFunctionD() {
+    var x = document.getElementById("planet_redmp3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+//Saturn
+function myFunctionE() {
+    var x = document.getElementById("saturnmp3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+//Wasserplanet
+function myFunctionF() {
+    var x = document.getElementById("wasserplanetmp3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
